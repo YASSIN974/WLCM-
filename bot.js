bcea63fdcf294d58302d5bb6e055d47709cac3c6
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2019-01-06 17:12:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ const w = ['./welcome.png'];
 })
 });
 
-const devs = ["439187325503930369","332713449215754242"]
+const devs = ["332713449215754242"]
 
 const adminprefix = "*";
 client.on('message', message => {
@@ -156,7 +156,7 @@ client.on('message', message => {
 
 client.on("guildMemberAdd", (member) => {
 
-let channel = "528306102815948820"
+let channel = "531502374947389441"
 
     if (!channel) {
 
@@ -178,8 +178,8 @@ let channel = "528306102815948820"
 
     while (!guild)
 
-        guild = client.guilds.get("508335358677876747");
-
+ guild = client.guilds.get("531502374947389441");
+	
     guild.fetchInvites().then((data) => {
 
         data.forEach((Invite, key, map) => {
@@ -209,7 +209,7 @@ if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let copy = "FeNiXo";
+let copy = "Fenixo.";
 let request = `Requested By ${message.author.username}`;
 if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
 msg.react('✅')
